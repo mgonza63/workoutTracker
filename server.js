a489ca027f38b6c937270c65804a28209b33d751
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-11 16:33:59 -0500

    created workout schema with mongoose

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true, useFindAndModify: false });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { useNewUrlParser: true, useFindAndModify: false });
 
 
 app.use(express.urlencoded({ extended: true }));
